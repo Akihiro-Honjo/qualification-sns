07ea5559f0f456ca4e5cf8589627a90fde233ce2
Akihiro-Honjo <akhr0711.h.a@gmail.com>
2026-07-07 22:12:18 +0900

    Add Qualification model

--- goals/admin.py
+++ goals/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Qualification
+
+
+@admin.register(Qualification)
+class QualificationAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "user",
+        "exam_date",
+        "target_hours",
+        "target_score",
+    )
+    search_fields = ("name",)
+    list_filter = ("exam_date",)
--- goals/models.py
+++ goals/models.py
@@ -1,3 +1,25 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+class Qualification(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="qualifications"
+    )
+    name = models.CharField("資格名", max_length=100)
+    exam_date = models.DateField("試験日")
+    target_hours = models.PositiveIntegerField("目標勉強時間", default=0)
+    target_score = models.PositiveIntegerField("目標点数", default=0)
+
+    created_at = models.DateTimeField("作成日時", auto_now_add=True)
+    updated_at = models.DateTimeField("更新日時", auto_now=True)
+
+    class Meta:
+        verbose_name = "資格"
+        verbose_name_plural = "資格"
+
+    def __str__(self):
+        return self.name
