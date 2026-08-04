6577e929f172e81e6ba8d88c1fa22a70076552ff
Akihiro-Honjo <akhr0711.h.a@gmail.com>
2026-08-04 22:02:12 +0900

    deploy prepare

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f6-an)svvy**m7l(wx*w@lml4%)0%9q#e()1f5p=uu&ob4)ujz'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "f6-an)svvy**m7l(wx*w@lml4%)0%9q#e()1f5p=uu&ob4)ujz"
+)
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
