ecd284bbbb6550db3778fb6b3e90e491c1c08396
Akihiro-Honjo <akhr0711.h.a@gmail.com>
2026-08-15 13:38:44 +0900

    add mypage

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.decorators import login_required
+from django.db.models import Sum
 from django.shortcuts import render, redirect
 
 from .forms import SignUpForm
-
+from goals.models import Qualification
+from studies.models import StudyRecord
 # Create your views here.
 
 def signup(request):
@@ -26,4 +29,53 @@ def signup(request):
         {
             "form": form
         },
+    )
+    
+
+# マイページ
+@login_required
+def profile(request):
+
+    qualifications = Qualification.objects.filter(
+        user=request.user
+    )
+
+    study_records = StudyRecord.objects.filter(
+        qualification__user=request.user
+    )
+
+    # 登録資格数
+    qualification_count = qualifications.count()
+
+    # 総学習時間
+    total_minutes = (
+        study_records.aggregate(
+            total=Sum("study_time")
+        )["total"]
+        or 0
+    )
+
+    total_hours = round(
+        total_minutes / 60,
+        1
+    )
+
+# 総学習日数
+    study_days = (
+        study_records
+        .values("study_date")
+        .distinct()
+        .count()
+    )
+
+    context = {
+        "qualification_count": qualification_count,
+        "total_hours": total_hours,
+        "study_days": study_days,
+    }
+
+    return render(
+        request,
+        "accounts/profile.html",
+        context,
     )
